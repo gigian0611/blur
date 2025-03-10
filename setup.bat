335fae6e1c84387005360cb6913bce970ebf7274
WIN-8EBV5MPENR7\hyeon <gigian0611@gmail.com>
2025-03-10 15:08:25 +0900

    setup.bat modified

--- setup.bat
+++ setup.bat
@@ -1,7 +1,8 @@
 git clone https://github.com/microsoft/vcpkg
 set root=%~dp0\vcpkg
-start %root%\bootstrap-vcpkg.bat
-pushd %root%
+cd /d %ROOT%
+call bootstrap-vcpkg.bat
+
 vcpkg install opencv4:x64-windows
 vcpkg integrate install
 pause
